21144cd3507745beb052756e6d28031810e6dbca
Darin McGill <darin.mcgill@gmail.com>
2013-03-16 16:51:15 -0500

    some cleanup, switched from c++ to c

--- setup.py
+++ setup.py
@@ -11,14 +11,9 @@ Popen("touch Root.pyx",shell=True).wait()
 ext = Extension(
     "cyPyon", 
     ["Root.pyx"],
-    language="c++",
-    #include_dirs=['/usr/local/lib/python2.7/site-packages/numpy/core/include'],
-    #extra_compile_args=[ '-std=c++0x' ],
-    pyrex_gdb=True,
 )
 
 setup( 
-    #name = 'Hello world app', 
     name = 'cyPyon', 
     ext_modules = [ext],
     cmdclass = {'build_ext': build_ext},
