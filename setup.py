6343ff21f824a23fe32ec0c437f94c73f28a5237
Darin McGill <darin.mcgill@gmail.com>
2018-03-04 03:07:17 -0500

    getting ready for dist

--- setup.py
+++ setup.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python
 from __future__ import print_function
-
-import sys
-print(sys.version)
-from distutils.core import setup
-from distutils.extension import Extension
+from distutils.core import setup, Extension
 from Cython.Distutils import build_ext
 from subprocess import Popen
 
-Popen("touch Root.pyx",shell=True).wait()
+Popen("touch Root.pyx", shell=True).wait()
 ext = Extension(
     "cyPyon", 
     ["Root.pyx"],
 )
 
 setup( 
-    name = 'cyPyon', 
-    ext_modules = [ext],
-    cmdclass = {'build_ext': build_ext},
+    name='cyPyon',
+    description='a cython based parser for python object notation',
+    url='https://github.com/darinmcgill/cyPyon',
+    version='0.0.0',
+    ext_modules=[ext],
+    author='Darin McGill',
+    keywords='pyon parser',
+    cmdclass={'build_ext': build_ext},
+    install_requires=['Cython'],
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
